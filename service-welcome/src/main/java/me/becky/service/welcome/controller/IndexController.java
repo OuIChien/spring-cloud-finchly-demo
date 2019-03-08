6126940d709009e54831ebb3ae0103825d107876
gary <ouichien@gmail.com>
2019-03-08 14:57:23 +0800

    init

---
+++ service-welcome/src/main/java/me/becky/service/welcome/controller/IndexController.java
@@ -0,0 +1,14 @@
+package me.becky.service.welcome.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "welcome this is service-welcome";
+    }
+
+}
